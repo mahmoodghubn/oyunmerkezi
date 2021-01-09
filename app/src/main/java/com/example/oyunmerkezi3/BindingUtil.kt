81fa07a6fa32b92b1531cd3056d5e6b721574dfa
mahmoodghubn <mahmood.ghubn@gmail.com>
2021-01-09 13:27:25 +0100

    using glide

--- app/src/main/java/com/example/oyunmerkezi3/BindingUtil.kt
+++ app/src/main/java/com/example/oyunmerkezi3/BindingUtil.kt
@@ -1,9 +1,10 @@
 package com.example.oyunmerkezi3
 
 import android.content.ContentValues.TAG
-import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.example.oyunmerkezi3.database.Game
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubeThumbnailLoader
@@ -26,46 +27,49 @@ fun TextView.setGameName(item: Game?) {
 }
 
 @BindingAdapter("videoThumbnailImageView")
-fun YouTubeThumbnailView.setThumbnail(item: Game?) {
+fun ImageView.setThumbnail(item: Game?) {
 
+    val URL = "https://i4.ytimg.com/vi/${item!!.URL[0]}/mqdefault.jpg"
     item?.let {
-        this.initialize(
-            "AIzaSyCbfkNTBYJp0JEp8hM4J0TCEm_EcnIvwig",
-            object : YouTubeThumbnailView.OnInitializedListener {
-                override fun onInitializationSuccess(
-                    youTubeThumbnailView: YouTubeThumbnailView,
-                    youTubeThumbnailLoader: YouTubeThumbnailLoader
-                ) {
-                    //when initialization is sucess, set the video id to thumbnail to load
-                    youTubeThumbnailLoader.setVideo(item.URL[0])
-                    youTubeThumbnailLoader.setOnThumbnailLoadedListener(object :
-                        OnThumbnailLoadedListener {
-                        override fun onThumbnailLoaded(
-                            youTubeThumbnailView: YouTubeThumbnailView,
-                            s: String
-                        ) {
-                            //when thumbnail loaded successfully release the thumbnail loader as we are showing thumbnail in adapter
-                            youTubeThumbnailLoader.release()
-                        }
+        Glide.with(this.context).load(URL).into(this)
 
-                        override fun onThumbnailError(
-                            youTubeThumbnailView: YouTubeThumbnailView,
-                            errorReason: YouTubeThumbnailLoader.ErrorReason
-                        ) {
-                            //print or show error when thumbnail load failed
-                            Log.e(TAG, "Youtube Thumbnail Error")
-                        }
-                    })
-                }
-
-                override fun onInitializationFailure(
-                    youTubeThumbnailView: YouTubeThumbnailView,
-                    youTubeInitializationResult: YouTubeInitializationResult
-                ) {
-                    //print or show error when initialization failed
-                    Log.e(TAG, "Youtube Initialization Failure")
-                }
-            })
+//        this.initialize(
+//            "AIzaSyCbfkNTBYJp0JEp8hM4J0TCEm_EcnIvwig",
+//            object : YouTubeThumbnailView.OnInitializedListener {
+//                override fun onInitializationSuccess(
+//                    youTubeThumbnailView: YouTubeThumbnailView,
+//                    youTubeThumbnailLoader: YouTubeThumbnailLoader
+//                ) {
+//                    //when initialization is sucess, set the video id to thumbnail to load
+//                    youTubeThumbnailLoader.setVideo(item.URL[0])
+//                    youTubeThumbnailLoader.setOnThumbnailLoadedListener(object :
+//                        OnThumbnailLoadedListener {
+//                        override fun onThumbnailLoaded(
+//                            youTubeThumbnailView: YouTubeThumbnailView,
+//                            s: String
+//                        ) {
+//                            //when thumbnail loaded successfully release the thumbnail loader as we are showing thumbnail in adapter
+//                            youTubeThumbnailLoader.release()
+//                        }
+//
+//                        override fun onThumbnailError(
+//                            youTubeThumbnailView: YouTubeThumbnailView,
+//                            errorReason: YouTubeThumbnailLoader.ErrorReason
+//                        ) {
+//                            //print or show error when thumbnail load failed
+//                            Log.e(TAG, "Youtube Thumbnail Error")
+//                        }
+//                    })
+//                }
+//
+//                override fun onInitializationFailure(
+//                    youTubeThumbnailView: YouTubeThumbnailView,
+//                    youTubeInitializationResult: YouTubeInitializationResult
+//                ) {
+//                    //print or show error when initialization failed
+//                    Log.e(TAG, "Youtube Initialization Failure")
+//                }
+//            })
     }
 }
 
